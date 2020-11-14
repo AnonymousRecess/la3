974eed9c7852e9d053aa2ad453b6ab9999041304
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-11-14 12:02:03 -0700

    fs

--- meetone.pl
+++ meetone.pl
@@ -6,13 +6,13 @@
 
 free(Person,slot(time(hour1,min1,id1), time(hour2,min1,id2))).
 
-lte(time(_,_, am), time(_,_, pm)).
+lte(time(_,_,am), time(_,_,pm)).
 
-lte(time(Hour1,_,_), time(Hour2, _,_)) :-
-Hour1 < Hour2.
+lte(time(Hour1,_,ID),time(Hour2, _,ID)) :-
+Hour1<Hour2.
 
 lte(time(Hour,Min1,ID), time(Hour, Min2, ID)) :-
-Min1 =< Min2.
+Min1=<Min2.
 
 
 meetone(Person,slot(TimeSeg1, TimeSeg2)) :- 
