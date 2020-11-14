f263982863e1d1729c476ceb345916d5fe9d012b
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-11-14 15:24:02 -0700

    df

--- query1.pl
+++ query1.pl
@@ -21,7 +21,6 @@ match(slot(ABegin,AEnd),slot(BBegin,BEnd),slot(BBegin,BEnd)) :-
 match(slot(ABegin,AEnd),slot(BBegin,BEnd),slot(BBegin,BEnd)) :-
     lte(ABegin,BBegin),
     lte(BBegin,AEnd),
-    lte(AEnd,BEnd),
     BBegin\==AEnd.
 
 meetCheck(ASlot,BSlot,SharedSlot) :-
