79a173ae38699889f7cf49bacf5cb970514ac52c
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-11-14 11:58:50 -0700

    sl

--- meetone.pl
+++ meetone.pl
@@ -4,14 +4,14 @@
 
 % Your code goes here.
 
-free(Person,slot(time(_,_,_), time(_,_,_))).
+free(Person,slot(time(hour1,min1,id1), time(hour2,min1,id2))).
 
 lte(time(_,_, am), time(_,_, pm)).
 
 lte(time(Hour1,_,_), time(Hour2, _,_)) :-
 Hour1 < Hour2.
 
-lte(time(_,Min1,_), time(_, Min2, _)) :-
+lte(time(Hour,Min1,ID), time(Hour, Min2, ID)) :-
 Min1 =< Min2.
 
 
