ac826d9fdc4d51f5186b86bf16d98e4fec4f82b1
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-11-14 12:15:13 -0700

    meetoneworks

--- meetone.pl
+++ meetone.pl
@@ -2,32 +2,30 @@
 
 :- include('data.pl').
 
-% Your code goes here.
 
-free(Person,slot(time(hour1,min1,id1),time(hour2,min1,id2))).
+free(Person,slot(time(hour1,min1,id1),time(hour2,min1,id2))). % Check for Person with Hour, Minute and Miday
 
 lte(time(_,_,am), time(_,_,pm)).
 
 lte(time(Hour1,_,ID),time(Hour2, _,ID)) :-
-		Hour1<Hour2.
+		Hour1<Hour2.	% Ensure that Hour1 is less than Hour2
 
 lte(time(Hour,Min1,ID), time(Hour, Min2, ID)) :-
-		Min1=<Min2.
+		Min1=<Min2.    % Ensure that Min1 is less than or equal to Min2
 
 
 meetone(Person,slot(Start, Finish)) :- 
-		free(Person,slot(TimeSeg1,TimeSeg2)),
-		lte(TimeSeg1,Start), lte(Finish, TimeSeg2).
+		free(Person,slot(TimeSeg1,TimeSeg2)), % Check for free time
+		lte(TimeSeg1,Start), lte(Finish, TimeSeg2).  % Check for overlap
 
 
 
 main :- findall(Person,
 		meetone(Person,slot(time(8,30,am),time(8,45,am))),
-		People),
-		write(People), nl, halt.
+		People), % compile People with overlapping free time
+		write(People), nl, halt. % print names of people who can meet
 
 :- initialization(main).
 
-% Need to print names of people who can meet
 
 % Would print Ann, Carla and Dave for 8:30-8:45
