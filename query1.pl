79af1656d459b3b4a0492588420ecac6ffae9a8a
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-11-16 13:50:36 -0700

    sfs

--- query1.pl
+++ query1.pl
@@ -5,22 +5,13 @@
 
 free(Person,slot(time(hour1,min1,id1),time(hour2,min1,id2))). % Check for Person with Hour, Minute and Miday
 
-lte(time(_,_,am), time(_,_,pm)).
 
-lte(time(Hour1,_,ID),time(Hour2, _,ID)) :-
-		Hour1<Hour2.	% Ensure that Hour1 is less than Hour2
 
-lte(time(Hour,Min1,ID), time(Hour, Min2, ID)) :-
-		Min1=<Min2.    % Ensure that Min1 is less than or equal to Min2
 
 match(slot(ABegin,AEnd),slot(BBegin,BEnd),slot(BBegin,BEnd)) :-
-    lte(ABegin,BBegin),
-    lte(BBegin,AEnd),
     BBegin\==AEnd.
 
 match(slot(ABegin,AEnd),slot(BBegin,BEnd),slot(BBegin,BEnd)) :-
-    lte(ABegin,BBegin),
-    lte(BBegin,AEnd),
     BBegin\==AEnd.
 
 meetCheck(ASlot,BSlot,SharedSlot) :-
