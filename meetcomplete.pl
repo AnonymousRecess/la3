8b5ccf4d25b7df9234756a899fa13bb51f81f482
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-11-16 21:14:33 -0700

    dffdsfs

--- meetcomplete.pl
+++ meetcomplete.pl
@@ -1,36 +1,36 @@
 :- include('data.pl').
+:- include('uniq.pl').
 
-free(Person,slot(time(hour1,min1,id1),time(hour2,min1,id2))).
+free(Person,slot(time(hour1,min1,id1),time(hour2,min1,id2))). % Check for Person with Hour, Minute and Miday
 
-lte(time(_,_,am), time(_,_,pm)). % can be am or pm
+lte(time(_,_,am), time(_,_,pm)).
 
-
-lte(time(Hour1,_,ID),time(Hour2,_,ID)) :-
+lte(time(Hour1,_,ID),time(Hour2, _,ID)) :-
 		Hour1<Hour2.	% Ensure that Hour1 is less than Hour2
-
 
 lte(time(Hour,Min1,ID), time(Hour, Min2, ID)) :-
 		Min1=<Min2.    % Ensure that Min1 is less than or equal to Min2
 
 
-overlap(slot(FirstStart,FirstEnd),slot(SecondStart,SecondEnd),slot(CurrentStart,CurrentEnd)) :- %match
+overlap(slot(FirstStart,FirstEnd),slot(SecondStart,SecondEnd),slot(SecondStart,SecondEnd)) :- %match
         lte(FirstStart,SecondStart),
-        lte(SecondStart,FirstStart),
-        FirstEnd\==SecondStart.
+        lte(SecondStart,FirstEnd),
+        lte(SecondEnd,FirstEnd),
+        FirstStart\==SecondEnd.
 
 meetCheck(FirstFreeTime,SecondFreeTime,SharedSlot) :-
         overlap(FirstFreeTime,SecondFreeTime,SharedSlot).
 
 
 compareNext([], FreeTime, FreeTime). % meetcollect
-compareNext([H|Tail], FirstFreeTime, FreeTime) :- % args are FreeTimes
-        free(H, SecondFreeTime), % Next person's free time
+compareNext([H2|Tail], FirstFreeTime, FreeTime) :- % args are FreeTimes
+        free(H2, SecondFreeTime), % Next person's free time
         overlap(FirstFreeTime,SecondFreeTime, CurrentSlot), % check if free times have overlap
         compareNext(Tail, CurrentSlot, FreeTime). % Pass rest of the list with recursion
 
 
-getFreeTimes([H|Tail], FreeTime) :- %meetTimes
-        free(H, FirstFreeTime),
+getFreeTimes([H1|Tail], FreeTime) :- %meetTimes
+        free(H1, FirstFreeTime),
         compareNext(Tail, FirstFreeTime, FreeTime).
 
 
