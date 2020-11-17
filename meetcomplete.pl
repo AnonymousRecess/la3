7bfa85054ceec256d301a7edffa852864d3e2d12
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2020-11-16 22:26:10 -0700

    l;op[q

--- meetcomplete.pl
+++ meetcomplete.pl
@@ -3,23 +3,23 @@
 
 
 
-free(Person,slot(time(hour1,min1,id1),time(hour2,min1,id2))). % Check for Person with Hour, Minute and Miday
+free(Person,slot(time(hour1,min1,id1),time(hour2,min1,id2))). 
 
 lte(time(_,_,am), time(_,_,pm)).
 
 lte(time(Hour1,_,ID),time(Hour2, _,ID)) :-
-		Hour1<Hour2.	% Ensure that Hour1 is less than Hour2
+		Hour1<Hour2.	
 
 lte(time(Hour,Min1,ID), time(Hour, Min2, ID)) :-
-		Min1=<Min2.    % Ensure that Min1 is less than or equal to Min2
+		Min1=<Min2.    
 
 
-overlap(slot(FirstStart,FirstEnd),slot(SecondStart,SecondEnd),slot(SecondStart,SecondEnd)) :- %match
+overlap(slot(FirstStart,FirstEnd),slot(SecondStart,SecondEnd),slot(SecondStart,SecondEnd)) :- 
         lte(FirstStart,SecondStart),
         lte(SecondStart,FirstEnd),
         lte(FirstEnd,SecondEnd),
         lte(SecondEnd,FirstEnd),
-        SecondStart=FirstEnd,
+        SecondStart\==FirstEnd,
         FirstStart\==SecondEnd.
 
 meetCheck(FirstFreeTime,SecondFreeTime,SharedSlot) :-
@@ -28,17 +28,17 @@ meetCheck(FirstFreeTime,SecondFreeTime,SharedSlot) :-
 meetCheck(FirstFreeTime,SecondFreeTime,SharedSlot) :-
         overlap(SecondFreeTime,FirstFreeTime,SharedSlot).
 
-compareNext([], FreeTime, FreeTime). % meetcollect
-compareNext([H2|Tail], FirstFreeTime, FreeTime) :- % args are FreeTimes
-        free(H2, SecondFreeTime), % Next person's free time
-        meetCheck(FirstFreeTime,SecondFreeTime, CurrentSlot), % check if free times have overlap
-        compareNext(Tail, CurrentSlot, FreeTime). % Pass rest of the list with recursion
+compareNext([], FreeTime, FreeTime). 
+compareNext([H2|Tail], FirstFreeTime, FreeTime) :- 
+        free(H2, SecondFreeTime), 
+        meetCheck(FirstFreeTime,SecondFreeTime, CurrentSlot), 
+        compareNext(Tail, CurrentSlot, FreeTime). 
 
-getFreeTimes([H1|Tail], FreeTime) :- %meetTimes
+getFreeTimes([H1|Tail], FreeTime) :- 
         free(H1, FirstFreeTime),
         compareNext(Tail, FirstFreeTime, FreeTime).
 
-collection(FreeTime) :-   % meet
+collection(FreeTime) :-  
         people(People),
         getFreeTimes(People, FreeTime).
 
